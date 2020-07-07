522fe685bb6beb90fbc1965eb0294ae1e46a50b6
Matthew Gerrard <matthew.gerrard@blackpepper.co.uk>
2020-07-07 21:40:00 +0100

    Second stage of carousel build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, QueryList, ViewChildren} from '@angular/core';
+import {gsap, Sine} from 'gsap';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  @ViewChildren('carouselItem')
+  private carouselItems : QueryList<ElementRef>;
+
+  public right(): void {
+    const carouselNativeElements = this.carouselItems.toArray().map(el => el.nativeElement);
+    const leftElement = carouselNativeElements[0];
+    const centerElement = carouselNativeElements[1];
+    const rightElement = carouselNativeElements[2];
+
+    gsap.timeline({ repeat: 0})
+      .to([centerElement], {
+        duration: 1,
+        ease: Sine.easeInOut,
+        x: '+=100%'
+      }).to([rightElement], {
+        delay: -1,
+        duration: 1,
+        ease: Sine.easeInOut,
+        x: '-=200%'
+      }).to([leftElement], {
+        delay: -1,
+        duration: 1,
+        ease: Sine.easeInOut,
+        x: '+=100%'
+      });
+  }
 }
