be4a00f0c9bc9b67d4601c38ae606204bb67365a
Matthew Gerrard <matthew.gerrard@blackpepper.co.uk>
2020-07-07 22:57:48 +0100

    Third stage of carousel build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,30 +8,45 @@ import {gsap, Sine} from 'gsap';
 })
 export class AppComponent {
 
+  public currentAvatarIndex = 1;
+
   @ViewChildren('carouselItem')
   private carouselItems : QueryList<ElementRef>;
 
   public right(): void {
+    const nextIndex = this.getPreviousIndex(this.currentAvatarIndex);
+
     const carouselNativeElements = this.carouselItems.toArray().map(el => el.nativeElement);
-    const leftElement = carouselNativeElements[0];
-    const centerElement = carouselNativeElements[1];
-    const rightElement = carouselNativeElements[2];
+    const moveToCenterElement = carouselNativeElements[nextIndex];
+    const moveToRightElement = carouselNativeElements[this.currentAvatarIndex];
+    const moveToLeftElement = carouselNativeElements[this.getNextIndex(this.currentAvatarIndex)];
 
     gsap.timeline({ repeat: 0})
-      .to([centerElement], {
+      .to([moveToRightElement], {
         duration: 1,
         ease: Sine.easeInOut,
         x: '+=100%'
-      }).to([rightElement], {
+      }).to([moveToLeftElement], {
         delay: -1,
         duration: 1,
         ease: Sine.easeInOut,
         x: '-=200%'
-      }).to([leftElement], {
+      }).to([moveToCenterElement], {
         delay: -1,
         duration: 1,
         ease: Sine.easeInOut,
         x: '+=100%'
+      }).eventCallback('onComplete', () => {
+        this.currentAvatarIndex = nextIndex;
       });
+  }
+
+  private getNextIndex(index: number): number {
+    return ((index + 1) % this.carouselItems.length);
+  }
+
+  private getPreviousIndex(index: number) {
+    return ((index + this.carouselItems.length - 1)
+      % this.carouselItems.length);
   }
 }
